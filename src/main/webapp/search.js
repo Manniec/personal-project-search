bbd4c94559bc435a3479dd769cb78d9576df8646
Diego Velazquez Alvarez <a01027322@itesm.mx>
2020-04-02 21:54:18 -0600

    Add search functionality that fetches the servlet with tags. Separate renderProject funtion from the fetch function. Add functionality on the servlet that checks for empty queries and gets the tags. Currently there is no project filtering in the servlet.

--- src/main/webapp/search.js
+++ src/main/webapp/search.js
@@ -1,59 +1,90 @@
-//Function that fetches all the projects from the servlet and displays them in cards (without any filtering):
-function getAllProjects(){
+//Function that renders the project cards:
+function renderProjects(projects){
 
-    fetch("/search").then(response => response.json()).then((projects) => {
+    //Get the div where the project cards are gonna be displayed:
+    const projectDisplay = document.getElementById("projects-display-section");
+    let currentProjectRow = document.createElement('div'); //Create the first row container.
+    let currentProjectRowContent = document.createElement('div'); //Create the first content container
+    //We need to keep a counter to know when to close a row and start a new one:
+    let counter = 0;
 
-        //Get the div where the project cards are gonna be displayed:
-        const projectDisplay = document.getElementById("projects-display-section");
-        let currentProjectRow = document.createElement('div'); //Create the first row container.
-        let currentProjectRowContent = document.createElement('div'); //Create the first content container
-        //We need to keep a counter to know when to close a row and start a new one:
-        let counter = 0;
+    //CSS style:
+    currentProjectRow.className = "projects-row";
+    currentProjectRowContent.className = "content-row";
 
-        //CSS style:
-        currentProjectRow.className = "projects-row";
-        currentProjectRowContent.className = "content-row";
+    //Iterates the JSON by keys:
+    for(let project of projects){
 
-        //Iterates the JSON by keys:
-        for(let project of projects){
+        counter++;
 
-            counter++;
+        //Create a card element with the current project values:
+        const currentProjectCard = buildProjectCard(project.title, project.tags, project.author, project.image);
 
-            //Create a card element with the current project values:
-            const currentProjectCard = buildProjectCard(project.title, project.tags, project.author, project.image);
+        //Add it to the current row:
+        currentProjectRowContent.appendChild(currentProjectCard);
 
-            //Add it to the current row:
-            currentProjectRowContent.appendChild(currentProjectCard);
-
-            //Only 3 elements per row:
-            if(counter % 3 == 0){
-
-                //Add the row to the DOM:
-                currentProjectRow.appendChild(currentProjectRowContent);
-                projectDisplay.appendChild(currentProjectRow);
-
-                //Create a new row and make it the current row:
-                currentProjectRow = document.createElement('div');
-                currentProjectRowContent = document.createElement('div');
-
-                //CSS style to the new row:
-                currentProjectRow.className = "projects-row";
-                currentProjectRowContent.className = "content-row";
-
-            }
-
-        }
-
-        //In case there is a number of projects that is not a multiple of 3, add the last row with the remaining cards:
-        if(counter % 3 != 0){
+        //Only 3 elements per row:
+        if(counter % 3 == 0){
 
             //Add the row to the DOM:
             currentProjectRow.appendChild(currentProjectRowContent);
             projectDisplay.appendChild(currentProjectRow);
 
+            //Create a new row and make it the current row:
+            currentProjectRow = document.createElement('div');
+            currentProjectRowContent = document.createElement('div');
+
+            //CSS style to the new row:
+            currentProjectRow.className = "projects-row";
+            currentProjectRowContent.className = "content-row";
+
         }
 
-    })
+    }
+
+    //In case there is a number of projects that is not a multiple of 3, add the last row with the remaining cards:
+    if(counter % 3 != 0){
+
+        //Add the row to the DOM:
+        currentProjectRow.appendChild(currentProjectRowContent);
+        projectDisplay.appendChild(currentProjectRow);
+
+    }
+
+}
+
+//Fetch the searched projects:
+function getProjects(){
+
+    const url = getURL();
+
+    fetch(url).then(response => response.json()).then((projects) => {
+
+        renderProjects(projects);
+
+    });
+
+}
+
+//Get the current url with the parameters:
+function getURL(){
+
+    const urlString = window.location.href;
+    const url = urlString.replace('.html', '');
+    
+    return url;
+
+}
+
+//Fill the search form and return it:
+function fillForm(bar){
+
+    const searchBar = document.getElementById("search-bar");
+    searchBar.value = bar;
+
+    const form = new FormData(document.getElementById("search-form"));
+
+    return form;
 
 }
 
