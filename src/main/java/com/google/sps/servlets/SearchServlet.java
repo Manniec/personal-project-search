677a829f182b6c03cb220d8d0c665046793b77db
Diego Velazquez Alvarez <a01027322@itesm.mx>
2020-04-07 23:40:26 -0500

    Add queryProjectDataStore function. Modify doGet to implement the last function. Add doPost method that posts dummy projects for test purposes. Addn form in HTML that uses this last method.

--- src/main/java/com/google/sps/servlets/SearchServlet.java
+++ src/main/java/com/google/sps/servlets/SearchServlet.java
@@ -8,13 +8,22 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
 import com.google.gson.Gson;
+import java.sql.Timestamp;
 
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
@@ -22,55 +31,200 @@ public class SearchServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
+        //Get the datastore instance:
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+        PreparedQuery results;
+
+        ArrayList<SearchProject> projects = new ArrayList<SearchProject>();
+
         //Check if there is any parameters on the request:
         if(request.getQueryString() != null){
 
+            //Query the datastore based on the request:
+            results = queryProjectDatastore(request, datastore);
+
+        } else {
+
+            //In case of an empty query: Return nothing or everything:
+            Query query = new Query("Project").addSort("timestamp", SortDirection.DESCENDING);
+    
+            //Query the datastore and get the results:
+            results = datastore.prepare(query);
+
+        }
+
+        for(Entity entity : results.asIterable()){
+
+            //Get the properties of the entity:
+            String title = (String) entity.getProperty("title");
+            String author = (String) entity.getProperty("author");
+            String collab = (String) entity.getProperty("collab");
+            String commitment = (String) entity.getProperty("commitment");
+            String difficulty = (String) entity.getProperty("difficulty");
+            String language = (String) entity.getProperty("language");
+            String zone = (String) entity.getProperty("zone");
+            String image = (String) entity.getProperty("image");
+
+            //Create an array of tags to be displayed:
+            String[] projectTagsList = {language, zone, difficulty, commitment, collab};
+
+            //Create a SearchProject object with the entity properties:
+            SearchProject currentProject = new SearchProject(title, projectTagsList, author, null); //Image is set to null for the moment as it displays the default project image.
+
+            //Add the current project to the ArrayList of projects:
+            projects.add(currentProject);
+
+        }
+
+        //Create a JSONString from the ArrayList:
+        String json = toJsonString(projects);
+
+        //Write the JSON to the response:
+        response.setContentType("application/json; charset=utf-8");
+        response.getWriter().println(json);
+
+    }
+
+    //This post method is only to put dummy entities in the datastore for test purposes:
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        //Get a datastore instance:
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+        //Create test Project Entity with dummy values:
+        Entity projectEntity = new Entity("Project");
+        projectEntity.setProperty("title", "Project Cars 3?");
+        projectEntity.setProperty("description", "This is a test description");    
+        projectEntity.setProperty("timestamp", System.currentTimeMillis());
+        projectEntity.setProperty("giturl", "https://github.com/Manniec/personal-project-search");
+        projectEntity.setProperty("author", "Manuel Doe"); //This can be the username or email.
+
+        //TAGS:
+        projectEntity.setProperty("language", "Spanish"); //In a future this can be an array of languages.
+        projectEntity.setProperty("zone", "Australia/Brisbane"); //Time Zone
+        projectEntity.setProperty("difficulty", "medium"); //Difficulty
+        projectEntity.setProperty("commitment", "more 9"); //Time commitment
+        projectEntity.setProperty("collab", "online"); //Collaboration type
+        projectEntity.setProperty("image", "default"); //This is optional. Set to "default" to tell the JS to display the placeholder project image.
+
+        //Put the Entity in the datastore:
+        datastore.put(projectEntity);
+
+        System.out.println("Dummy project posted!");
+
+        // Redirect back to the HTML search page.
+        response.sendRedirect("/search.html");
+
+    }
+
+    private PreparedQuery queryProjectDatastore(HttpServletRequest request, DatastoreService datastore){
+
             //Get parameters for the search:
             String searchBarText = request.getParameter("bar");
-            String language = request.getParameter("tag-zones");
+            String language = request.getParameter("tag-languages");
+            String zone = request.getParameter("tag-zones");
             String difficulty = request.getParameter("difficulties");
             String timeCommitment = request.getParameter("time-commitment");
             String collabType = request.getParameter("collab-type");
 
-        }
+            //ArrayList of query subfilters for the composite filter:
+            Collection filters = new ArrayList<FilterPredicate>();
 
-        //Dummy Data:
-        ArrayList<SearchProject> projectData = new ArrayList<SearchProject>();
+            //Store the results of the query:
+            PreparedQuery results;
 
-        String[] project1List = {"Tag1", "Tag2", "Tag3", "Tag4"};
-        SearchProject project1 = new SearchProject("Test Project 1", project1List, "Test Author", null);
+            //Debug prints:
+            System.out.println("searchBar = " + searchBarText);
+            System.out.println("language = " + language);
+            System.out.println("zone = " + zone);
+            System.out.println("difficulty = " + difficulty);
+            System.out.println("time = " + timeCommitment);
+            System.out.println("collabType = " + collabType);
 
-        String[] project2List = {"English", "Pacific/Midway", "Easy", "Online"};
-        SearchProject project2 = new SearchProject("Test Project 2", project2List, "John Doe", null);
+            //Create and add query filters for each tag based on the request parameters:
+            if(!"".equals(searchBarText)){
 
-        String[] project3List = {"Spanish", "America/Mexico_City", "Hard", "Online"};
-        SearchProject project3 = new SearchProject("Test Project 3", project3List, "Diego Velázquez", null);
+                //TODO: Implement free text search filter here.
+                System.out.println("The search bar is not empty");
 
-        String[] project4List = {"German", "America/Los_Angeles", "Impossible", "Presential"};
-        SearchProject project4 = new SearchProject("Test Project 4", project4List, "Toto Wolf", null);
+            }
 
-        String[] project5List = {"English", "US/Eastern", "Medium", "Offline"};
-        SearchProject project5 = new SearchProject("Test Project 5", project5List, "Danika Vanderlienden", null);
+            if(!"".equals(language)){
 
-        String[] project6List = {"Japanese", "Asia/Tokyo", "Hard", "Online"};
-        SearchProject project6 = new SearchProject("Test Project 6", project6List, "Takumi Fujiwara", null);
+                //Property name, comparison operator, search value:
+                System.out.println("Language is not empty");
+                filters.add(new FilterPredicate("language", FilterOperator.EQUAL, language));
 
-        String[] project7List = {"Japanese", "Asia/Tokyo", "Hard", "Online"};
-        SearchProject project7 = new SearchProject("Test Project 6", project7List, "Takumi Fujiwara", null);
+            }
 
-        projectData.add(project1);
-        projectData.add(project2);
-        projectData.add(project3);
-        projectData.add(project4);
-        projectData.add(project5);
-        projectData.add(project6);
-        projectData.add(project7);
-        projectData.add(project7);
+            if(!"".equals(zone)){
 
-        String json = toJsonString(projectData);
+                System.out.println("Zone is not empty");
+                filters.add(new FilterPredicate("zone", FilterOperator.EQUAL, zone));
 
-        response.setContentType("application/json; charset=utf-8");
-        response.getWriter().println(json);
+            }
+
+            if(difficulty != null){
+
+                System.out.println("Difficulty is not empty");
+                filters.add(new FilterPredicate("difficulty", FilterOperator.EQUAL, difficulty));
+
+            }
+
+            if(timeCommitment != null){
+
+                System.out.println("Commitment is not empty");
+                filters.add(new FilterPredicate("commitment", FilterOperator.EQUAL, timeCommitment));
+
+            }
+
+            if(collabType != null){
+
+                System.out.println("Collab is not empty");
+                filters.add(new FilterPredicate("collab", FilterOperator.EQUAL, collabType));
+
+            }
+
+            //Check for single filter queries and empty queries:
+            System.out.println(filters.size());
+
+            if(filters.size() == 0) {
+
+                //In case of an empty query: Return nothing or everything:
+                Query query = new Query("Project").addSort("timestamp", SortDirection.DESCENDING);
+    
+                //Query the datastore and get the results:
+                results = datastore.prepare(query);
+
+            } else if(filters.size() == 1) {
+                
+                Iterator<FilterPredicate> iterator = filters.iterator();
+
+                Filter queryFilter = iterator.next();
+
+                //Create a query with the single filter:
+                Query query = new Query("Project").setFilter(queryFilter).addSort("timestamp", SortDirection.DESCENDING);
+    
+                //Query the datastore and get the results:
+                results = datastore.prepare(query);
+
+            } else {
+
+                //Create a composite filter with all the previous filters:
+                Filter queryFilter = new CompositeFilter(CompositeFilterOperator.AND, filters);
+
+                //Create a query with the composite filter and sort:
+                Query query = new Query("Project").setFilter(queryFilter).addSort("timestamp", SortDirection.DESCENDING);
+    
+                //Query the datastore and get the results:
+                results = datastore.prepare(query);
+
+            }
+
+            //Return the results:
+            return results;
 
     }
 
